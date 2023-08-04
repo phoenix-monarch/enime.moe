77aa354b1288d78383a6d3014f23a255822e6e10
NADESHIKON <allenzhang040817@gmail.com>
2023-08-03 23:54:51 -0700

    Trolled

--- components/ad/index.tsx
+++ components/ad/index.tsx
@@ -5,5 +5,6 @@ export default function Ad() {
         {/*
         <Script async strategy="afterInteractive" crossOrigin="anonymous" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2103276838446130"/>
         */}
+        <Script async strategy="afterInteractive" crossOrigin="anonymous" src='https://pl20236888.highcpmrevenuegate.com/c5/c3/b7/c5c3b75fcf91040957b2bbe8d2e4349f.js'/>
     </>
 }
